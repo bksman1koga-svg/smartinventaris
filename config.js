b11e801d61bea8931a086d188105ab3e6a41f536
bksman1koga-svg <bksman1koga@gmail.com>
2026-09-25 17:18:11 +0700

    Update config.js
    gntii

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// KONFIGURASI PRODUKSI
-// Jangan bungkus kode ini dengan tanda kutip tambahan.
 window.APP_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbyFkhZHRGhnHqQ00vdrpIWaV8VYmouc3Fi53QOE2LkGxkDoSg44fwmX2G3u2vUh8zhLZg/exec",
   APP_VERSION: "2.1.0"
